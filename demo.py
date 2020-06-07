5d4d732b2b5b24dd3a08435e0e284e2d2adfa59a
Kristof <kristof.kovacs@telenav.com>
2020-06-07 13:20:47 +0300

    new version

--- demo.py
+++ demo.py
@@ -1,13 +1,32 @@
+from enum import Enum
+import json
+
 from to_dict import Dictable
 
+class E(Enum):
+    test = 'test_val'
+
+class D:
+    def __init__(self):
+        self.val = 'str_val'
+
+class C:
+    def __init__(self):
+        self.d1 = D()
+        self.d2 = D()
+
 class B(Dictable):
-    def __init__(self, val: int):
-        self.val = val
+    def __init__(self, vals: list):
+        self.vals = vals
 
 class A(Dictable):
     def __init__(self):
         self.a = 5
         self.b = 'b'
-        self.version2 = B(999)
+        self.c = E.test
+        self.d = C()
+        self.e = [B([996, 997]), B([998, 999])]
+        self.f = {'key_1': B([996, 997]), 'key_2': B([998, 999])}
 
 print(A().dict)
+print(json.dumps(A().json, indent=4))
