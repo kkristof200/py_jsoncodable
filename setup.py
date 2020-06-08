f5dc1c2307b8d270d4f6c52d77e7797c35b7b62a
Kristof <kristof.kovacs@telenav.com>
2020-06-08 12:55:38 +0300

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setuptools.setup(
     name="jsoncodable",
-    version="0.0.2",
+    version="0.0.3",
     author="Kristof",
     description="to_dict",
     long_description=long_description,
